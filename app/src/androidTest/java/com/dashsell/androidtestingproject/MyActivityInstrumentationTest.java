92606564cd7a05ead58da3889f472a9eb92bf7a5
andy ccs <andychong2@gmail.com>
2014-07-08 15:36:55 +0800

    add Robotium to the testing

--- app/src/androidTest/java/com/dashsell/androidtestingproject/MyActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/dashsell/androidtestingproject/MyActivityInstrumentationTest.java
@@ -1,6 +1,12 @@
 package com.dashsell.androidtestingproject;
 
+import android.app.Instrumentation;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.UiThreadTest;
+import android.util.Log;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.jayway.android.robotium.solo.Solo;
@@ -28,6 +34,7 @@ public class MyActivityInstrumentationTest extends ActivityInstrumentationTestCa
     public void setUp() throws Exception {
         super.setUp();
         solo = new Solo(getInstrumentation(), getActivity());
+        setActivityInitialTouchMode(false);
     }
 
 
@@ -36,8 +43,58 @@ public class MyActivityInstrumentationTest extends ActivityInstrumentationTestCa
         TextView textView = (TextView) solo.getView(R.id.hello_world);
         String text = textView.getText().toString();
 
-        assertEquals("They are not equal","Hello World!",text);
+        assertEquals("They are not equal","Hello world!",text);
     }
 
+    /**
+     * this test will fail
+     * @throws Exception
+     */
+    public void testSaveState() throws Exception {
+        MyActivity activity = getActivity();
 
+        //set the edit text box to 1
+        EditText editText = solo.getEditText(0);
+        solo.clearEditText(editText);
+        solo.enterText(editText, "1");
+
+        int restoreStateNumber;
+
+        Log.d("TEST","stateNumber: " + 1);
+
+        //restart the activity
+        activity.finish();
+        setActivity(null);
+        activity = this.getActivity();
+
+        //check whether the state is being save
+        restoreStateNumber = activity.getStateNumber();
+
+        Log.d("TEST","restoreStateNumber: " + restoreStateNumber);
+
+        //test
+        assertEquals("The state is not saved",1,restoreStateNumber);
+    }
+
+    /**
+     * this test will pass
+     * @throws Exception
+     */
+    public void testStatePause() throws Exception{
+        Instrumentation instrumentation = getInstrumentation();
+        MyActivity activity = getActivity();
+
+        //set the edit text box to 1
+        EditText editText = solo.getEditText(0);
+        solo.clearEditText(editText);
+        solo.enterText(editText, "1");
+
+        //change the orientation
+        solo.setActivityOrientation(Solo.LANDSCAPE);
+        solo.setActivityOrientation(Solo.PORTRAIT);
+
+        //test
+        assertEquals("The state is not saved", 1, activity.getStateNumber());
+
+    }
 }
--- app/src/main/java/com/dashsell/androidtestingproject/MyActivity.java
+++ app/src/main/java/com/dashsell/androidtestingproject/MyActivity.java
@@ -2,18 +2,96 @@ package com.dashsell.androidtestingproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+
+import java.util.Random;
 
 
 public class MyActivity extends Activity {
+    public static final String TAG = MyActivity.class.getSimpleName();
+    public static final String KEY = "state number key";
+    public static final int INVALID_STATE_NUMBER = -1;
+    private int stateNumber;
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        Log.d(TAG,"onCreate");
+
+        editText = (EditText) findViewById(R.id.editText);
+        if(savedInstanceState!=null){
+            stateNumber = savedInstanceState.getInt(KEY,INVALID_STATE_NUMBER);
+
+        }else {
+            stateNumber = 0;
+        }
+
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart");
+//        stateNumber = 1;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume");
+        editText.setText(String.valueOf(stateNumber));
+//        stateNumber = 2;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause");
+        stateNumber = Integer.valueOf(editText.getEditableText().toString());
+//        stateNumber = 3;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop");
+//        stateNumber = 4;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy");
+//        stateNumber = 5;
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG,"onRestart");
+//        stateNumber = 6;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG,"onSaveInstanceState");
+        outState.putInt(KEY, stateNumber);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d(TAG,"onRestoreInstanceState");
+        stateNumber = savedInstanceState.getInt(KEY,INVALID_STATE_NUMBER);
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -32,5 +110,13 @@ public class MyActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public int getStateNumber() {
+        return stateNumber;
+    }
+
+    public void setStateNumber(int stateNumber) {
+        this.stateNumber = stateNumber;
     }
 }
